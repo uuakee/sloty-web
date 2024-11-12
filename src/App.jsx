3d1124546ed073ff67c8644b64a503cba245bab9
joao-gallo <contato@kaueguedes.com.br>
2024-11-12 17:14:39 -0300

    add: scrollbar home

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import './App.css'
 import Header from './components/Header'
 import bannerImg from '/banner.jpg'
-import jackpotImg from '/jacktop.png'
-
 
 function App() {
   return (
@@ -12,9 +10,15 @@ function App() {
         <div className='px-2'>
           <img src={bannerImg} alt="" className='rounded-xl'/>
         </div>
-        <div className='mt-5'>
-          {/* <img src={jackpotImg} alt="" /> */}
+        <div className='mt-2 px-2'>
+          <div className='scrolling-text bg-background-600 text-sm p-2 text-white rounded-lg'>
+            <span>
+              As recompensas VIP da semana serão distribuídas às 21h, na segunda-feira, dia 11 de novembro. 
+              Não perca a oportunidade de garantir sua recompensa e vencer com as recompensas VIP.
+            </span>
+          </div>
         </div>
+        
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,23 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        background: {
+          50: "#E8E8E8",
+          100: "#D1D1D1",
+          200: "#A3A3A3",
+          300: "#757575",
+          400: "#474747",
+          500: "#181818",
+          600: "#141414",
+          700: "#0F0F0F",
+          800: "#0A0A0A",
+          900: "#050505",
+          950: "#030303",
+        },
+      },
       fontFamily: {
-        schibsted: ['Schibsted Grotesk', 'Helvetica', 'sans-serif'], // Adicione a fonte aqui
+        schibsted: ['Schibsted Grotesk', 'Helvetica', 'sans-serif'],
       },
     },
   },
